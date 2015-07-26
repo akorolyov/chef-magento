d3b840caa5de67039e580a52d4f138f5bb318c6a
Andrey Korolyov <akorolyov@mavenecommerce.com>
2015-07-26 18:04:35 -0400

    add php configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,38 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "php"
+
+
+package 'php' do
+  action :install
+
+  # install the mongodb pecl
+  php_pear "mongo" do
+    action :install
+  end
+
+  # install the xdebug pecl
+  php_pear "xdebug" do
+    # Specify that xdebug.so must be loaded as a zend extension
+    zend_extensions ['xdebug.so']
+    action :install
+  end
+
+  # install apc pecl with directives
+  php_pear "apc" do
+    action :install
+    directives(:shm_size => 128, :enable_cli => 1)
+  end
+
+  # Install a FPM pool named "default"
+  php_fpm_pool "default" do
+    action :install
+  end
+
+end
+
+service 'php-fpm' do
+  action [ :enable, :start ]
+end
