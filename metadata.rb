db0e6e329222e21a00b20720127fff7e490217b1
Andrey Korolyov <akorolyov@mavenecommerce.com>
2015-07-26 17:56:39 -0400

    init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'magento'
+maintainer       'MavenEcommerce Inc'
+maintainer_email 'akorolyov@mavenecommerce.com'
+license          'All rights reserved'
+description      'Installs/Configures magento'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: magento
+# Recipe:: default
+#
+# Copyright 2015, MavenEcommerce Inc
+#
+# All rights reserved - Do Not Redistribute
+#
