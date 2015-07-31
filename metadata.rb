b1e1ec7b7ddd0b18b9c7b8b0314aa17e8d530a71
Andrey Korolyov <akorolyov@mavenecommerce.com>
2015-07-31 19:17:58 -0400

    test

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures magento'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'php', '>= 1.7.0'
