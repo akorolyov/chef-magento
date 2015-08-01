fc44c3a43e1c41373aee5fe6bfce17cda5a002d3
Andrey Korolyov <akorolyov@mavenecommerce.com>
2015-07-31 20:37:48 -0400

    test

---
+++ recipes/mysql.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: magento
+# Recipe:: mysql
+#
+# Copyright 2015, MavenEcommerce Inc
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "mysql"
+
+mysql_service 'default' do
+  port '3306'
+  version '5.6'
+  initial_root_password ''
+  action [:create, :start]
+end
+
+mysql_client 'default' do
+  action :create
+end
---
+++ recipes/php.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: magento
+# Recipe:: php
+#
+# Copyright 2015, MavenEcommerce Inc
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe "php"
+
+# install the mongodb pecl
+php_pear "mongo" do
+  action :install
+end
+
+#install the xdebug pecl
+php_pear "xdebug" do
+  # Specify that xdebug.so must be loaded as a zend extension
+  zend_extensions ['xdebug.so']
+  action :install
+end
+
+# Install a FPM pool named "default"
+php_fpm_pool "default" do
+  action :install
+end
+
+service 'php5-fpm' do
+  action [ :enable, :start ]
+end
