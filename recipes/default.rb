a3a5e3affbd0ef97cb92ba95b55b5c7f2849e5ef
Andrey Korolyov <akorolyov@mavenecommerce.com>
2015-08-14 10:49:18 -0400

    test

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,15 @@
 include_recipe 'magento::php'
 include_recipe 'magento::mysql'
 include_recipe 'magento::nginx'
+
+execute "create #{node[:mysql][:database][:name]} database" do
+    command <<-EOH
+    /usr/bin/mysqladmin -u root -h 127.0.0.1 create #{node[:mysql][:database][:name]}
+    EOH
+end
+
+execute "import data for #{node[:mysql][:database][:name]} database" do
+    command <<-EOH
+    /usr/bin/mysql -u root -h 127.0.0.1 #{node[:mysql][:database][:name]} < #{node[:magento][:application][:directory]}/source/#{node[:mysql][:database][:name]}.sql
+    EOH
+end
--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-mysql_service 'default' do
+mysql_service 'mysql' do
   port '3306'
   bind_address '0.0.0.0'
   version '5.6'
@@ -15,18 +15,6 @@ mysql_service 'default' do
   action [:create, :start]
 end
 
-mysql_client 'default' do
+mysql_client 'mysql' do
   action :create
-end
-
-execute "create #{node[:mysql][:database][:name]} database" do
-    command <<-EOH
-    /usr/bin/mysqladmin -u root -h 127.0.0.1 create #{node[:mysql][:database][:name]}
-    EOH
-end
-
-execute "import data for #{node[:mysql][:database][:name]} database" do
-    command <<-EOH
-    /usr/bin/mysql -u root -h 127.0.0.1 #{node[:mysql][:database][:name]} < #{node[:magento][:application][:directory]}/source/#{node[:mysql][:database][:name]}.sql
-    EOH
 end
